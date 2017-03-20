9b7b16c76576bc6daedbba68916f7551cb51ecb4
Antonin <antonin.renaudier@gmail.com>
2017-03-20 21:09:04 +0100

    Amélioration aspirante

--- Python/aspirante_player.py
+++ Python/aspirante_player.py
@@ -2,10 +2,10 @@ from aspirante import aspirante
 
 
 class AspirantePlayer:
-    def __init__(self, depth, evaluation,alpha,beta):
+    def __init__(self, depth, evaluation):
         self.depth = depth
-        self.alpha = alpha
-        self.beta = beta
+        self.alpha = -48
+        self.beta = 48
         self.evaluation = evaluation
 
     def get_move_rec(self, awale, player):
